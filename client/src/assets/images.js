034cb12878510de39e344e7d61bb7dd93afac727
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-05-11 01:55:48 +0300

    test PR #19

---
+++ client/src/assets/images.js
@@ -0,0 +1,21 @@
+import VRSectorHero from './images/home/vr-engineer-robotics-hero.jpg';
+import AboutImage from './images/home/about-image.png';
+import HeroImage from './images/events/events-hero.jpg';
+import AiLaptopImage from './images/events/ai-laptop.jpg';
+import WorkplaceImage from './images/events/workplace.jpg';
+import TableImage from './images/events/table.jpg';
+import TeamImage from './images/events/team.jpg';
+import RegistrationImage from './images/events/registration.jpg';
+import MeetingImage from './images/events/meeting.png';
+
+export {
+  VRSectorHero,
+  AboutImage,
+  HeroImage,
+  AiLaptopImage,
+  WorkplaceImage,
+  TableImage,
+  TeamImage,
+  RegistrationImage,
+  MeetingImage,
+};
